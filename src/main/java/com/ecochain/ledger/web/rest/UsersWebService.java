9a5b75b9d7af462bf20eadadd1ef4ae4b4ffdc68
zhangchunming <543483026@qq.com>
2017-06-08 15:28:51 +0800

    添加区块浏览器

--- src/main/java/com/ecochain/ledger/web/rest/UsersWebService.java
+++ src/main/java/com/ecochain/ledger/web/rest/UsersWebService.java
@@ -203,6 +203,7 @@ public class UsersWebService extends BaseWebService {
     @ApiOperation(nickname = "用户注册", value = "用户注册", notes = "用户注册")
     @ApiImplicitParams({
         @ApiImplicitParam(name = "account", value = "登陆账号，仅支持手机号注册", required = true, paramType = "query", dataType = "String"),
+        @ApiImplicitParam(name = "user_name", value = "用户名", required = true, paramType = "query", dataType = "String"),
         @ApiImplicitParam(name = "password", value = "密码，6-16位数字", required = true, paramType = "query", dataType = "String")
     })
     public AjaxResponse register(HttpServletRequest request,HttpServletResponse response){
@@ -214,6 +215,7 @@ public class UsersWebService extends BaseWebService {
             logger.info("--------------register  pd value is "+pd.toString());
             String account = StringUtil.isEmpty(pd.getString("account"))?null:pd.getString("account").trim();
             String password = StringUtil.isEmpty(pd.getString("password"))?null:pd.getString("password").trim();
+            String user_name = StringUtil.isEmpty(pd.getString("user_name"))?null:pd.getString("user_name").trim();
             if(StringUtil.isEmpty(account)){
                 ar.setSuccess(false);
                 ar.setMessage("请输入登陆账号！");
@@ -224,6 +226,12 @@ public class UsersWebService extends BaseWebService {
                 ar.setSuccess(false);
                 ar.setMessage("账号格式不正确！");
                 ar.setErrorCode(CodeConstant.MOBILE_ERROR);
+                return ar;
+            }
+            if(StringUtil.isEmpty(user_name)){
+                ar.setSuccess(false);
+                ar.setMessage("用户名不能为空！");
+                ar.setErrorCode(CodeConstant.PARAM_ERROR);
                 return ar;
             }
             /*if(!Validator.isAccountByLetterAndNum(account)){
@@ -257,7 +265,7 @@ public class UsersWebService extends BaseWebService {
             pd.put("account", account);
             pd.put("user_type", 1);//买家
             pd.put("mobile_phone", account);//买家
-            pd.put("user_name", account);//买家
+            pd.put("user_name", user_name);//买家
             pd.put("status", "1");//会员状态默认启用
             pd.put("password", password);
             pd.put("lastlogin_ip", InternetUtil.getRemoteAddr(request));
